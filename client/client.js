42887061243d52824fe92b00575df023b5ada07a
Bruno Lemos <brunohplemos@gmail.com>
2014-11-11 22:09:59 -0200

    Fixed Publish -- Now it's safer and reactive

--- client/client.js
+++ client/client.js
@@ -1,4 +1,13 @@
-Meteor.subscribe("users");
-Meteor.subscribe("projects");
-Meteor.subscribe("tasks");
-Meteor.subscribe("companies");
+Meteor.subscribe("allCompanies");
+
+Deps.autorun(function() {
+	try {
+		var myCompanies = Meteor.user().profile.companies;
+	} catch(e) {
+		var myCompanies = null;
+	}
+
+	Meteor.subscribe("usersFromMyCompanies", myCompanies);
+	Meteor.subscribe("projectsFromMyCompanies", myCompanies);
+	Meteor.subscribe("tasksFromMyCompanies", myCompanies);
+});
--- server/server.js
+++ server/server.js
@@ -1,24 +1,34 @@
-Meteor.publish("users", function() {
-	var companyId = Meteor.users.findOne(this.userId).profile.companies[0];
-	return Meteor.users.profile.find({companies: companyId});
+Meteor.publish("allCompanies", function() {
+	return Companies.find();
 });
 
-Meteor.publish("projects", function() {
-	if(this.userId)
-	{
-		try {
-			var companyId = Meteor.users.findOne(this.userId).profile.companies[0];
-			return Projects.find({companyId: companyId});	
-		} catch(e) {}
+Meteor.publish("usersFromMyCompanies", function(companies) {
+	if(companies instanceof Array) {
+		return Meteor.users.find({'profile.companies': { $in: companies }});
 	}
 
 	return null;
 });
 
-Meteor.publish("tasks", function() {
-	return Tasks.find();
+Meteor.publish("projectsFromMyCompanies", function(companies) {
+	if(companies instanceof Array) {
+		return Projects.find({companyId: { $in: companies }});
+	}
+
+	return null;
 });
 
-Meteor.publish("companies", function() {
-	return Companies.find();
+Meteor.publish("tasksFromMyCompanies", function(companies) {
+	if(companies instanceof Array) {
+		var projectIds = [];
+		var projects = Projects.find({companyId: { $in: companies }}).fetch();
+
+		for(var i = 0; i < projects.length; i++) {
+			projectIds.push(projects[i]._id);
+		}
+
+		return Tasks.find({projectId: { $in: projectIds }});
+	}
+
+	return null;
 });
